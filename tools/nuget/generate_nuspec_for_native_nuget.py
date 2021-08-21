2672bf246d5d381f0298249bd56b497287b3712f
Changming Sun <chasun@microsoft.com>
2021-08-20 23:19:37 -0700

    update

--- tools/nuget/generate_nuspec_for_native_nuget.py
+++ tools/nuget/generate_nuspec_for_native_nuget.py
@@ -371,16 +371,17 @@ def generate_files(list, args):
                           runtimes_target + args.target_architecture + '\\native" />')
     
     if is_cpu_package or is_cuda_gpu_package:
-        nuget_artifacts_dir = Path(args.native_build_path) / 'nuget-artifacts')
-        for child in nuget_artifacts_dir.iterdir():
-            for cpu_arch in ['x86', 'x64', 'arm', 'arm64']:
-                if child.name == 'onnxruntime-win-%s' % arch:
-                    files_list.append('<file src=' + '"' + child/'lib' / 'onnxruntime.dll' +
-                                   ' target="runtimes/win-%s/native/onnxruntime.dll"/>' % cpu_arch)
-                    files_list.append('<file src=' + '"' + child/'lib' / 'onnxruntime.pdb' +
-                                   ' target="runtimes/win-%s/native/onnxruntime.pdb"/>' % cpu_arch)
-                    files_list.append('<file src=' + '"' + child/'lib' / 'onnxruntime.lib' +
-                                   ' target="runtimes/win-%s/native/onnxruntime.lib"/>' % cpu_arch)
+        nuget_artifacts_dir = Path(args.native_build_path) / 'nuget-artifacts'
+        if nuget_artifacts_dir.exists():
+            for child in nuget_artifacts_dir.iterdir():
+                for cpu_arch in ['x86', 'x64', 'arm', 'arm64']:
+                    if child.name == 'onnxruntime-win-%s' % arch:
+                        files_list.append('<file src=' + '"' + child/'lib' / 'onnxruntime.dll' +
+                                       ' target="runtimes/win-%s/native/onnxruntime.dll"/>' % cpu_arch)
+                        files_list.append('<file src=' + '"' + child/'lib' / 'onnxruntime.pdb' +
+                                       ' target="runtimes/win-%s/native/onnxruntime.pdb"/>' % cpu_arch)
+                        files_list.append('<file src=' + '"' + child/'lib' / 'onnxruntime.lib' +
+                                       ' target="runtimes/win-%s/native/onnxruntime.lib"/>' % cpu_arch)
             
     
     # process all other library dependencies
