5dbb5d577dafd42fb76cd4e5a2f4c8535c58bdd3
Changming Sun <chasun@microsoft.com>
2021-08-20 22:14:10 -0700

    update

--- tools/ci_build/github/windows/bundle_dlls.ps1
+++ tools/ci_build/github/windows/bundle_dlls.ps1
@@ -1,9 +1,9 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-$ort_dirs = Get-ChildItem -Path $Env:BUILD_BINARIESDIRECTORY\RelWithDebInfo\RelWithDebInfo\nuget-artifacts\Microsoft.ML.OnnxRuntime*.nupkg  | Where-Object { !( $_ | Select-String "Managed" -quiet) }
-
-if($ort_dirs.Length -ne 1)
+$ort_dirs = Get-ChildItem -Name -Path Microsoft.ML.OnnxRuntime*.nupkg  | Where-Object { ($_ -notmatch "Microsoft.ML.OnnxRuntime.Managed.*") }
+Write-Output ($ort_dirs | Measure-Object).Count
+if(($ort_dirs | Measure-Object).Count -ne 1)
 {
 	throw "length mismatch"
 }
