835f95a6ae7aa6a3020c718500c8e7e2487ffe5e
Changming Sun <chasun@microsoft.com>
2021-08-20 21:36:23 -0700

    update

---
+++ tools/ci_build/github/windows/bundle_dlls.ps1
@@ -0,0 +1,33 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+$ort_dirs = Get-ChildItem -Path $Env:BUILD_BINARIESDIRECTORY\RelWithDebInfo\RelWithDebInfo\nuget-artifacts\Microsoft.ML.OnnxRuntime*.nupkg  | Where-Object { !( $_ | Select-String "Managed" -quiet) }
+
+if($ort_dirs.Length -ne 1)
+{
+	throw "length mismatch"
+}
+foreach ($nuget_file in $ort_dirs)
+{
+	mkdir runtimes\win-x86\native
+	mkdir runtimes\win10-arm\native
+	mkdir runtimes\win10-arm64\native
+	mkdir runtimes\linux-x64\native
+	mkdir runtimes\linux-aarch64\native
+	mkdir runtimes\osx.10.14-x64\native
+	mkdir runtimes\osx.10.14-arm64\native
+	Rename-Item -Path onnxruntime-win-x86\lib\onnxruntime.dll -NewName runtimes\win-x86\native\onnxruntime.dll
+	Rename-Item -Path onnxruntime-win-x86\lib\onnxruntime.lib -NewName runtimes\win-x86\native\onnxruntime.lib
+	Rename-Item -Path onnxruntime-win-x86\lib\onnxruntime.pdb -NewName runtimes\win-x86\native\onnxruntime.pdb
+	Rename-Item -Path onnxruntime-win-arm64\lib\onnxruntime.dll -NewName runtimes\win10-arm64\native\onnxruntime.dll
+	Rename-Item -Path onnxruntime-win-arm64\lib\onnxruntime.lib -NewName runtimes\win10-arm64\native\onnxruntime.lib
+	Rename-Item -Path onnxruntime-win-arm64\lib\onnxruntime.pdb -NewName runtimes\win10-arm64\native\onnxruntime.pdb
+	Rename-Item -Path onnxruntime-win-arm\lib\onnxruntime.dll -NewName runtimes\win10-arm\native\onnxruntime.dll
+	Rename-Item -Path onnxruntime-win-arm\lib\onnxruntime.lib -NewName runtimes\win10-arm\native\onnxruntime.lib
+	Rename-Item -Path onnxruntime-win-arm\lib\onnxruntime.pdb -NewName runtimes\win10-arm\native\onnxruntime.pdb
+	Rename-Item -Path onnxruntime-linux-x64\lib\libonnxruntime.so.1* -NewName runtimes\linux-x64\native\libonnxruntime.so
+	Rename-Item -Path onnxruntime-linux-aarch64\lib\libonnxruntime.so.1* -NewName runtimes\linux-aarch64\native\libonnxruntime.so
+	Rename-Item -Path onnxruntime-osx-x64\lib\libonnxruntime.*.dylib -NewName runtimes\osx.10.14-x64\native\libonnxruntime.dylib
+	Rename-Item -Path onnxruntime-osx-arm64\lib\libonnxruntime.*.dylib -NewName runtimes\osx.10.14-arm64\native\libonnxruntime.dylib
+	7z a  $nuget_file runtimes
+}
